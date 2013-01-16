0b2f4fa48d22178fd05f652ac231be043401a348
wov <wovfeng@gmail.com>
2013-01-16 11:43:09 +0800

    add server

---
+++ static.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname+ '/../'));
+app.listen(3000);
+console.log('server start at ' + __dirname +',@port 3000');
